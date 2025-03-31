797a37a05955686fe66c44362e123f761f3b87ed
Chris Herzig <chrisherzig@msn.com>
2025-03-31 12:51:58 -0400

    Setting up Open AI API chat

---
+++ Server/Services/IOpenAIAPI.cs
@@ -0,0 +1,11 @@
+﻿using AiChef.Shared;
+
+namespace AiChef.Server.Services
+{
+    public interface IOpenAIAPI
+    {
+        Task<List<Idea>> CreateRecipeIdeas(string mealtime, List<string> ingredients);
+        // This method will call the OpenAI API to generate recipe ideas based on the provided meal time and ingredients.
+
+    }
+}
---
+++ Server/Services/OpenAIService.cs
@@ -0,0 +1,12 @@
+﻿using AiChef.Shared;
+
+namespace AiChef.Server.Services
+{
+    public class OpenAIService : IOpenAIAPI
+    {
+        public Task<List<Idea>> CreateRecipeIdeas(string mealtime, List<string> ingredients)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
